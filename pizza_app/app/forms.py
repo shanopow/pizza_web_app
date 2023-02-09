9aff108288d41620a036be9a1336eb2803211ca8
shanopow <115043900+shanopow@users.noreply.github.com>
2023-02-09 15:39:11 +0000

    Updated comments on forms.py

--- pizza_app/app/forms.py
+++ pizza_app/app/forms.py
@@ -23,24 +23,28 @@ class CustomerForm(forms.ModelForm):
         data_m = dat[0:2]
         data_y = dat[3:]
         
-        # too short
+        # too short, cant be too long as form is char limited to 5
         if len(str(dat)) != 5:
             raise forms.ValidationError('Expiry Date is too short')
         
-        # format
+        # expiry
         elif dat[2] != '/':
             raise forms.ValidationError('Missing "/" between month and year of expiry' )
         
+        # expiry
         elif data_m not in allowed_months:
             raise forms.ValidationError('Month is invalid')
         
         elif len(str(data['cvv'])) != 3:
             raise forms.ValidationError('Invalid CVV') 
 
+        # expiry
         elif len(str(data['card'])) > 19 or len(str(data['card'])) < 4:
             raise forms.ValidationError('Invalid Card Number')
 
+        # expiry, nasty way, change
         try:
+            # make sure give an int
             data_y = int(data_y)
         except:
             raise forms.ValidationError('Year is invalid')
